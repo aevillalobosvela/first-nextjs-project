49a272b3f8c89a0cb5d02838499f5fa8df88850c
Alvaro Villalobos Vela <vivealed@gmail.com>
2025-09-23 22:42:03 -0400

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Link from "next/link";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,6 +27,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>
+          <Link href={"/"}>Home</Link>
+          <Link href={"/about"}>About</Link>
+        </div>
         {children}
       </body>
     </html>
